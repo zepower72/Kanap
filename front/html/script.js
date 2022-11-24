63a3da635f0e8dffe4e54f17b6f570beb3723168
zepower72 <114823832+zepower72@users.noreply.github.com>
2022-11-24 11:46:33 +0100

    Api page d'accueil corrigée

--- front/html/script.js
+++ front/html/script.js
@@ -1,36 +1,41 @@
 // Page d'accueil
 // Affichage des produits dans la section "items".
 const sectionItems = document.querySelector('#items');
+function createProductHTML(products) {
 
-// Récupération de toutes les données de l'api avec la constante listProducts
+    // Création des éléments html incluants les données de l'api
+    let newA = document.createElement('a');
+    newA.setAttribute("href", `./product.html?id=${products._id}`);
+    sectionItems.appendChild(newA);
+
+    let newArticle = document.createElement('article');
+    newA.appendChild(newArticle);
+
+    let newImg = document.createElement('img');
+    newImg.setAttribute("src", products.imageUrl);
+    newImg.setAttribute("alt", products.altTxt);
+    newArticle.appendChild(newImg);
+
+    let newH3 = document.createElement('h3');
+    newH3.setAttribute("class", "productName");
+    newH3.innerText = products.name;
+    newArticle.appendChild(newH3);
+
+    let newP = document.createElement('p');
+    newP.setAttribute("class", "productDescription");
+    newP.innerText = products.description;
+    newArticle.appendChild(newP);
+
+    return newA;
+}
+// Récupération de toutes les données de l'api avec la constante products
 fetch("http://localhost:3000/api/products")
     .then(response => response.json())
     .then(data => {
-        for (const listProducts of data) {
-            
+        for (const product of data) {
+            let prodHTML = createProductHTML(product)
+            sectionItems.appendChild(prodHTML)
 
-            // Création des éléments html incluants les données de l'api
-            let newA = document.createElement('a');
-            newA.setAttribute("href", `./product.html?id=${listProducts._id}`);
-            sectionItems.appendChild(newA);
-
-            let newArticle = document.createElement('article');
-            newA.appendChild(newArticle);
-
-            let newImg = document.createElement('img');
-            newImg.setAttribute("src", listProducts.imageUrl);
-            newImg.setAttribute("alt", listProducts.altTxt);
-            newArticle.appendChild(newImg);
-
-            let newH3 = document.createElement('h3');
-            newH3.setAttribute("class", "productName");
-            newH3.innerText = listProducts.name;
-            newArticle.appendChild(newH3);
-
-            let newP = document.createElement('p');
-            newP.setAttribute("class", "productDescription");
-            newP.innerText = listProducts.description;
-            newArticle.appendChild(newP);
         }
     })
     .catch(err => {
